866969b307f272eaafcd839ced27b7d986b4cf8d
William Vladimir Alvarado Campos <william.alvaradoc@outlook.com>
2018-10-26 13:38:25 -0600

    Unit 2 Examples

---
+++ Input - Output/HelloWorld.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<conio.h>
+#include<cstdlib>
+
+using namespace std;
+
+#define CONSTANTE 1.25
+
+int main()
+{
+	int num1=11, num2=20, num3=30;
+	for(;;num1++)
+	{
+		cout<<num1<<endl;
+		if(num1==30)
+			break;
+	}
+	system("pause");
+}
+
+
---
+++ Input - Output/SimpleInputOutputCStyle.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  string name;
+  printf("Hello, what's your name? \n");
+  scanf("%s", &name);
+  printf("Hi %s, nice to meet you", &name);
+}
+
+
+
