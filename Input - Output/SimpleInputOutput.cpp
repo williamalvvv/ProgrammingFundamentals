4d92f02b378401fbdabd1b8404bae2d8d8c9c86d
William Vladimir Alvarado Campos <william.alvaradoc@outlook.com>
2018-10-26 14:09:28 -0600

    Unit 2: Examples Modified

--- Input - Output/SimpleInputOutput.cpp
+++ Input - Output/SimpleInputOutput.cpp
@@ -7,7 +7,7 @@ int main()
   string name;
   cout<<"Hello, What's your name?"<<endl;
   cin>>name;
-  cout<<"Hi "<<name<<", nice to meet you \n";
+  cout<<"Hi "<<name<<", nice to meet you! \n";
 }
 
 
--- Input - Output/SimpleInputOutputCStyle.cpp
+++ Input - Output/SimpleInputOutputCStyle.cpp
@@ -7,7 +7,7 @@ int main()
   string name;
   printf("Hello, what's your name? \n");
   scanf("%s", &name);
-  printf("Hi %s, nice to meet you", &name);
+  printf("Hi %s, nice to meet you!", &name);
 }
 
 
