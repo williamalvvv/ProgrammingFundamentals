e5f2061450070a688d0d5ad6aa88894cc52c1281
William Vladimir Alvarado Campos <william.alvaradoc@outlook.com>
2018-10-30 21:36:00 -0600

    do while C++ Programs Added

---
+++ do while/ZeroToNCounter.cpp
@@ -0,0 +1,22 @@
+//Preprocesor directive for basic input output manipulation
+#include <iostream>
+//Using C++ standard namespace functions
+using namespace std;
+
+//Program body
+//This program will print a series of numbers from 0 to user input, only if user input number is less than 100
+int main(){
+	int num; //This variable will keep input from user
+	cout<<"Please, input a number for me to count to: ";
+	cin>>num;
+	int i=0;
+	if(num<100){
+		do{
+			cout<<i<<endl;
+			i++;
+		}while(i<num);
+	}
+	else{
+		cout<<"Number can't be equals or greater than 100'"<<endl;
+	}
+}
