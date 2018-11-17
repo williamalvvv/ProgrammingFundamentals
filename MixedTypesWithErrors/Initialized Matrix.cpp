4df28716dc3ad5e25f6debee71b22f1c5b64fc0c
William Vladimir Alvarado Campos <william.alvaradoc@outlook.com>
2018-11-17 08:09:58 -0600

    Array Matrix Mixed Types Errors

---
+++ MixedTypesWithErrors/Initialized Matrix.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int vector[2][2]={{"string",'a'},{true,4}};
+	cout<<"Values in Matrix:\n\n";
+	for(int i=0;i<2;i++)
+	{
+		for(int j=0;j<2;j++)
+		{
+			cout<<"2D array value at position ["<<i<<","<<j<<"]: "<<vector[i][j]<<"\n";
+		}
+	}
+	cout<<"\n\nMatrix Formated Output:\n\n";
+	for(int i=0;i<2;i++)
+	{ 
+		for(int j=0;j<33;j++)
+		{
+			cout<<"-";
+		}
+		cout<<"\n";
+		cout<<"|";
+		for(int j=0;j<2;j++)
+		{
+			cout<<"\t"<<vector[i][j]<<"\t|";
+		}
+		cout<<"\n";
+	}
+		for(int j=0;j<33;j++)
+		{
+			cout<<"-";
+		}
+	cout<<"\n\n";
+}
