653d01f45d9e9a4d38f5273749750ac111158735
William Alvarado <william.alvaradoc@outlook.com>
2018-12-03 13:08:48 -0800

    comments were added to this example

--- String type manipulation/char pointer examples/char pointer example.cpp
+++ String type manipulation/char pointer examples/char pointer example.cpp
@@ -7,13 +7,13 @@ int main()
     // pointer str points to const string literal "Hello". No need to declare size. 
     char* str = "This is char* string example"; 
     cout << str << endl; 
-    int size = 30; 
-    // can allocate size dynamically. 
+    int size = 40; 
+    // can allocate a fixed size dynamically, in this case, it locates 40 bytes in memory 
     char* str2 = (char*)malloc(sizeof(char) * size); 
-    str2 = "GeeksForGeeks For Everyone"; 
+    str2 = "This is just another example"; 
     cout << str2; 
-  	char* str3;
-  	cin>>str3;
-  	cout<<str3;
+  	char* str3; //This creates an empty string that cannot be changed, due to constant restrictions
+  	cin>>str3; //when user tries to input string 3 value program trows an runtime error
+  	cout<<str3; //this line never executes due to error
     return 0; 
 } 
