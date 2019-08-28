b75cff0897abbcd0ac6ac430bfe297a93787d5d0
William Alvarado <william.alvaradoc@outlook.com>
2019-08-28 15:58:25 -0600

    Project resources were updated

--- Final Project Resources/Taxi queue control Tabbed with Functions.cpp
+++ Final Project Resources/Taxi queue control Tabbed with Functions.cpp
@@ -61,38 +61,13 @@ int main(){
 				printTable(taxis, drivers, plates);
 			break;
 			case 3:
-				cout<<"Really wanna send first avialable taxi? (Y/N): ";
-				confirmation=getch();
-				cout<<"\n";
-				if(confirmation=='Y' || confirmation=='y')
-				{
-					//Adding Items to non avaialable vectors
-					nAtaxis.push_back(taxis.at(0));
-					nAdrivers.push_back(drivers.at(0));
-					nAplates.push_back(plates.at(0));
-					//Deleting items from avaialable arrays
-					taxis.erase(taxis.begin());
-					drivers.erase(drivers.begin());
-					plates.erase(plates.begin());
-				}
-				printTable(taxis, drivers, plates);
+				//code logic for sending taxis here
 			break;
 			case 4:
-				cout<<"Taxis in route: "<<endl;
-				printTable(nAtaxis, nAdrivers, nAplates);
+				//code logic for on route checks
 			break;
 			case 5:
-				cout<<"Which taxi would you like to reinsert? "<<endl;
-				printTable(nAtaxis, nAdrivers, nAplates);
-				cout<<"\nPlease, input order below: ";
-				cin>>idToReinsert;
-				taxis.push_back(nAtaxis.at(idToReinsert));
-				drivers.push_back(nAdrivers.at(idToReinsert));
-				plates.push_back(nAplates.at(idToReinsert));
-				nAtaxis.erase(nAtaxis.begin()+idToReinsert);
-				nAdrivers.erase(nAdrivers.begin()+idToReinsert);
-				nAplates.erase(nAplates.begin()+idToReinsert);
-				cout<<"Taxi has been already inserted to avialable queue"<<endl;
+				//code logic for reinset taxis into avaialable queue
 			break;
 			case 6:
 				cout<<"Program has ended...";
